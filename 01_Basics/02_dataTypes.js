5231d1e3e522c3c5b4b56105b51ab4b77929d078
YogindraChaudhari <70800128+YogindraChaudhari@users.noreply.github.com>
2024-02-29 10:15:01 +0000

    container config done

--- 01_Basics/02_dataTypes.js
+++ 01_Basics/02_dataTypes.js
@@ -3,3 +3,23 @@
 //alert("1 + 1") //It will work in browser only
 
 console.log(3 + 3)
+
+let name = "Yogindra"
+let age = 18
+let isLoggedIn = true
+
+// ranges of datatype:
+// number = 2 ^ 53
+// bigInt = for the storage of large data
+// string = " " 
+// boolean = Y/N or t/f
+// null = standalone value, representation of empty value eg. zero is also the temperature so while initializing it can be safe to use null.
+let state = null
+//undefined = 
+//symbol = used in react for finding uniqueness
+
+
+//object
+console.log(typeof "me")
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # js-tutorial
 Javascript Learning.
 ECMA script documentation: https://tc39.es/
+MDN Documentation: https://developer.mozilla.org/en-US/docs/Web/javascript
