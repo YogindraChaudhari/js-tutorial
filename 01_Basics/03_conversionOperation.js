20554b1eae9a8d088a04069e438313d6c1c4eb50
YogindraChaudhari <70800128+YogindraChaudhari@users.noreply.github.com>
2024-03-01 11:33:22 +0000

    Conversion Operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -37,3 +37,23 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+// ************************************Operations***********************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "Good"
+let str2 = " Morning"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+console.log(1 + "2" + 2)
+console.log("1" + "2" + 2)
+console.log(1 + "2" + "2")
