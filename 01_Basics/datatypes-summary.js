a6534d8be6116e58934751a24e68b4ebe35427d2
YogindraChaudhari <70800128+YogindraChaudhari@users.noreply.github.com>
2024-03-02 10:31:26 +0000

    Working of memory

--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -11,7 +11,7 @@ let userEmail
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId) //symbols are unique, so never be the same
+// console.log(id === anotherId) //symbols are unique, so never be the same
 
 const bigNumber = 4789347384793778789n
 
@@ -29,6 +29,32 @@ const myFunction = function(){
     console.log("Hello World")
 }
 
-console.log(typeof bigNumber)
+// console.log(typeof bigNumber)
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// ********************************************************************************************************
+
+// Memory in javascript
+
+// Stack(primitive) and Heap(non-premitive)
+
+// from stack = copy, heap = reference/changes will be reflected in original value
+
+let myname = "name1"
+let anothername = myname
+anothername = "name2"
+console.log(myname);
+console.log(anothername)
+
+
+let user1 = {
+    email: "user@gmail.com",
+    upi: "user@ktk"
+}
+
+let user2 = user1
+user2.email = "user@yahoo.com"
+
+console.log(user1);
+console.log(user2);
