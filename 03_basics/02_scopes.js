6e2f522e8e299e543bed209f66e26c727f105039
YogindraChaudhari <70800128+YogindraChaudhari@users.noreply.github.com>
2024-03-04 13:28:27 +0000

    scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -7,7 +7,32 @@ if (true) {
     const b = 20
     console.log("Inner: ", a)
 }
-console.log(a);
-console.log(b);
+// console.log(a);
+// console.log(b);
 
 //global scope of node in browser and IDE is different 
+
+
+//Nested Scope:
+
+function one(){
+    const username = "Yogindra"
+    function two(){
+        const website = "youtube"
+        console.log(username)
+    }
+    // console.log(website)
+    two()
+}
+one()
+
+if (true) {
+    const username = "Yogindra"
+    if (username === "Yogindra") {
+        const website = " youtube"
+        console.log(username + website)
+    }
+    console.log(website)
+}
+
+console.log(username)
