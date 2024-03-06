cea9b83f3efdf2373a7b102e9f34b8996988d4a7
YogindraChaudhari <70800128+YogindraChaudhari@users.noreply.github.com>
2024-03-06 12:40:13 +0000

    Working of Switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,43 @@
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("Jan")
+        break;
+    case 2:
+        console.log("Feb")
+        break;
+    case 3:
+        console.log("March")
+        break;
+    case 4:
+        console.log("Apr")
+        break;
+    case 5:
+        console.log("May")
+        break;
+    case 6:
+        console.log("Jun")
+        break;
+
+    default:
+        console.log("Default Case")
+        break;
+}
+
+const Month = "Mar"
+switch (Month) {
+    case "Jan":
+        console.log("Jan")
+        break;
+    case "Mar":
+        console.log("Mar")
+        break;
+    case "May":
+        console.log("May")
+        break;
+
+    default:
+        console.log("Another Case")
+        break;
+}
