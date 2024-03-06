3c7b93c998aa559529a6562e3b079ffc352047b4
YogindraChaudhari <70800128+YogindraChaudhari@users.noreply.github.com>
2024-03-06 10:59:21 +0000

    immediately inovked function expression

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,13 @@
+//Immediately Invoked Function Expressions (IIFE)
+
+(function chai(){
+    console.log("DB connected")
+})();
+
+// ( () => {
+//     console.log('DB connected two')
+// })
+
+( (name) => {
+    console.log(`DB connected ${name}`);
+} )('Hitesh')
