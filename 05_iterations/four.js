c815612edd2bba319e2945399947d0043d89dff1
YogindraChaudhari <70800128+YogindraChaudhari@users.noreply.github.com>
2024-03-07 13:34:51 +0000

    for-each

---
+++ 05_iterations/four.js
@@ -0,0 +1,45 @@
+// for each
+const coding = ['js', 'ruby', 'cpp', 'c', 'py']
+coding.forEach( function (item) {
+    console.log(`Extensions are ${item}`)
+} )
+
+coding.forEach(function (val) {
+    console.log(val)
+} )
+
+coding.forEach((e) => {
+    console.log(e)
+})
+
+function printMe(item){
+    console.log(item)
+}
+coding.forEach(printMe)
+
+coding.forEach(function(item, index, array){
+    console.log(item, index, array)
+})
+
+// most important in Database
+
+const myCoding = [
+    {
+    langName : "javascript",
+    langExt: "js"
+    },
+    {
+    langName : "c++",
+    langExt: "cpp"
+    },
+    {
+    langName : "python",
+    langExt: "py"
+    },
+
+]
+
+myCoding.forEach((item) => {
+    // console.log(item.langName)
+    console.log(`Extension of ${item.langName} is .${item.langExt}`)
+})
